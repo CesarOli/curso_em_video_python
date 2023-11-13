cc274b3692016ef58d10161d6d220bbf3e79197b
CesarOli <cesaraleoliveira@gmail.com>
2023-11-12 22:29:11 -0300

    Desafio dois.

---
+++ Desafios Python Guanabara/desafio2.py
@@ -0,0 +1,5 @@
+dia = input('Qual o dia do seu nascimento? ')
+mês = input('Qual o mês do seu nascimento? ')
+ano = input('Qual o mês do seu nascimento? ')
+
+print('Sua data de nascimento é',dia,'de', mês,'de',ano,',correto?')
