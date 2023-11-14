2b8a01d9b7c2ed4bd2bd06780db5da1cd21d4888
CesarOli <cesaraleoliveira@gmail.com>
2023-11-13 23:56:11 -0300

    desafio sete.

---
+++ Desafios Python Guanabara/desafio7.py
@@ -0,0 +1,6 @@
+nota1 = float(input('Digite a nota da sua primeira prova: '))
+nota2 = float(input('Digite a nota da sua segunda prova: '))
+
+media = (nota1 + nota2) / 2
+
+print('Sua média é {}' .format(media))
