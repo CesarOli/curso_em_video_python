3834632d2357058c42e1eb394144d758ce07cf1e
CesarOli <cesaraleoliveira@gmail.com>
2023-11-13 23:55:41 -0300

    Desafio seis.

---
+++ Desafios Python Guanabara/desafio6.py
@@ -0,0 +1,6 @@
+n = int(input('informe um número qualquer: '))
+#d = n * 2
+#t = n * 3
+#q = n ** (1/2)
+
+print('o dobro de {} é {}, \no triplo de {} é {}, \nraiz quadrada de {} é {:.2f}.'.format(n, n*2, n, n*3, n, pow(n, (1/2))))
