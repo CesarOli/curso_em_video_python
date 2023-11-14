53d3af0fd4b4d5af192f8ec90c32c340b3c7dd01
CesarOli <cesaraleoliveira@gmail.com>
2023-11-13 23:54:20 -0300

    Desadio quatro.

---
+++ Desafios Python Guanabara/desafio4.py
@@ -0,0 +1,10 @@
+c = input("digite algo: ")
+
+print('O tipo primitivo de algo é ', type(c))
+print('É alfanumérico?' ,c.isalnum())
+print('É letra do alfabeto? ',c.isalpha())
+print('É um número decimal? ',c.isdecimal())
+print('Está em letras minúsculas? ',c.islower())
+print('Está em letras maiuscúlas? ',c.isupper)
+
+
