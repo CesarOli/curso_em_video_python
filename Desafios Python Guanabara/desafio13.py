422c39f499bd64ecc0aa8d365ab8134ae7a6d531
CesarOli <cesaraleoliveira@gmail.com>
2023-11-14 23:53:06 -0300

    Desafio treze.

---
+++ Desafios Python Guanabara/desafio13.py
@@ -0,0 +1,4 @@
+salario = float(input('Informe o seu salário atual: R$ '))
+novoSalario = salario + (salario * 15 / 100)
+
+print('Seu novo salário depois do aumento será de {:.2f} '.format(novoSalario))
