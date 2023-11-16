eacfbfe30ea745b4e10da00482812f3d7fd10683
CesarOli <cesaraleoliveira@gmail.com>
2023-11-15 22:57:53 -0300

    Desafio dezesseis.

---
+++ Desafios Python Guanabara/desafio16.py
@@ -0,0 +1,6 @@
+from math import trunc
+numero = float(input('Informe um número real: '))
+print('A parte inteira do numero real é {}.'.format(trunc(numero)))
+
+num = float(input('Informe um número real: '))
+print('O valor digitado foi {} e a sua parte inteira é {}'.format(num, int(num)))
