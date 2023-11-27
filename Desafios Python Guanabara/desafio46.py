4a3f62daf1a170cb512eb8936f57e363e48e026e
CesarOli <cesaraleoliveira@gmail.com>
2023-11-27 00:01:39 -0300

    Desafio quarenta e seis.

---
+++ Desafios Python Guanabara/desafio46.py
@@ -0,0 +1,9 @@
+from time import sleep
+print('\033[32mContagem Regressiva.\033[m')
+for anoNovo in range(10, -1, -1):
+    sleep(1)
+    print(anoNovo)
+print('BOWMM, BOWMM, BOWMM!!')
+print('FELIZ ANO NOOOVOOO')
+sleep(2)
+print('SEEU TROOOXA!! AHAHAHAHHA')
