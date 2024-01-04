cbd1b339c78ae4520acdeddd34c2628b2b5f7fb9
CesarOli <cesaraleoliveira@gmail.com>
2024-01-04 09:04:43 -0300

    Desafio noventa e quatro.

--- Desafios Python Guanabara/desafio94.py
+++ Desafios Python Guanabara/desafio94.py
@@ -6,57 +6,44 @@
 # - Uma lista com todas as mulheres 
 # - Uma lista com a todas as pessoas com idade acima da média 
 
-'''pessoa = {}
+pessoa = {}
 cadastro = []
-media = 0
-
+media = soma = 0
 
 while True:
-    pessoa['Nome'] = str(input('Digite aqui o seu nome: '))
-
-    pessoa ['sexo'] = str(input('Digite aqui seu sexo, "F" ou "M": ' )).upper().strip()
+    pessoa.clear()
+    pessoa['nome'] = str(input('Digite seu nome: '))
 
     while True:
-        pessoa['sexo'] = input('Digite aqui seu sexo, "F" ou "M": ').upper().strip()
-        
-        if pessoa['sexo'] not in 'M' or 'F':
+        pessoa['sexo'] = str(input('Digite seu sexo (M/F): ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
             break
-        else:
-            print('Por favor, digite somente "F" ou "M".')
-
-    pessoa['idade'] = int(input('Digite sua idade: '))
+        print('ERRO: Por favor, digite somente M ou F: ')
+    pessoa ['idade'] = int(input('Digite sua idade: '))
+    soma += pessoa['idade']
     cadastro.append(pessoa.copy())
-    resposta = str(input('Deseja continuar: S/N: ')).strip().upper()
-    if resposta == 'N':
+    while True:
+        resposta = str(input('Deseja encerrar o programa? [S/N]')).upper()[0]
+        if resposta in 'SN':
+            break
+        print('ERRO!! Responda apenas S ou N.')
+    if resposta == 'S':
         break
-media = media + pessoa['idade'] / len(cadastro)
-print(media)
-
-print(f"No total foram cadastradas {len(cadastro)} pessoas.")'''
-
-pessoas = []
-soma_idades = 0
-
-while True:
-    pessoa = {}
-    pessoa['nome'] = input('Digite o nome da pessoa (ou "sair" para encerrar): ')
-    
-    if pessoa['nome'].lower() == 'sair':
-        break
-    
-    pessoa['sexo'] = input('Digite o sexo da pessoa (M/F): ')
-    pessoa['idade'] = int(input('Digite a idade da pessoa: '))
-    
-    pessoas.append(pessoa)
-    soma_idades += pessoa['idade']
-
-quantidade_pessoas = len(pessoas)
-media_idade = soma_idades / quantidade_pessoas if quantidade_pessoas > 0 else 0
-
-mulheres = [pessoa['nome'] for pessoa in pessoas if pessoa['sexo'].lower() == 'f']
-acima_media = [pessoa for pessoa in pessoas if pessoa['idade'] > media_idade]
-
-print(f'\nQuantidade de pessoas cadastradas: {quantidade_pessoas}')
-print(f'Média de idade do grupo: {media_idade:.2f}')
-print(f'Lista de mulheres: {mulheres}')
-print(f'Pessoas com idade acima da média: {[pessoa["nome"] for pessoa in acima_media]}')
+     
+print(cadastro)
+print(f'Ao todo temos {len(cadastro)} pessoas cadastradas.')
+media = soma / len(cadastro)
+print(f'A média de idade é de {media:5.2f} anos.')
+print('As mulheres cadastradas foram ', end='')
+for p in cadastro:
+    if p['sexo']in 'Ff':
+        print(f'{p["nome"]} ', end='')
+print()
+print('Lista das pessoas que estão acima da média: ')
+for p in cadastro:
+    if p['idade'] >= media:
+        print('    ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('<< PROGRAMA ENCERRADO!!')
