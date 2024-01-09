e7da753402a498782cded6812705c3a595fd2db5
CesarOli <cesaraleoliveira@gmail.com>
2024-01-08 23:54:55 -0300

    Atualização do Desafio noventa e cinco.

--- Desafios Python Guanabara/desafio95.py
+++ Desafios Python Guanabara/desafio95.py
@@ -1,19 +1,19 @@
 #Aprimore o desafio 093 para que ele funcione com vários jogadores, incluindo
 #um sistema de visualização de detalhes do aproveitamento de cada jogador.
-time = []
+time = list()
 jogador = dict()
-gols = []
-partidas = ()
+partidas = list()
 
 while True:
     jogador.clear()
     jogador['nome'] = str(input('Nome do Artilheiro: '))
     quantidade_de_partidas = int(input(f'Quantas partidas {jogador["nome"]}o artilheiro jogou?: '))
+    partidas.clear()
 
     for g in range(0, quantidade_de_partidas):
-        gols.append(int(input(f'Quantos gols na partida {g+1}: ')))
-    jogador['Gols'] = partidas[:]
-    jogador['total'] = sum(gols)
+        partidas.append(int(input(f'Quantos partidas na partida {g+1}? ')))
+    jogador['partidas'] = partidas[:]
+    jogador['total'] = sum(partidas)
     resposta = str(input('Deseja continuar? [S/N]: ')).upper()[0]
     time.append(jogador.copy())
 
@@ -25,8 +25,8 @@ while True:
         break
 print('=' * 50)
 for i, k in enumerate(time):
-    print(f' {k:>3}', end='')
-    for d in i.values():
+    print(f' {i+1:>4} ', end='')
+    for d in k.values():
         print(f'{str(d):<15}', end='')
     print()
 print('-'*40)
