679099cd4786ea880f2b7d281efa8f12125a336e
CesarOli <cesaraleoliveira@gmail.com>
2024-04-24 16:41:14 -0300

    Desafio 97 finalizado.

--- Desafios Python Guanabara/desafio97.py
+++ Desafios Python Guanabara/desafio97.py
@@ -1,3 +1,17 @@
 #Faça um programa que tenha uma função chamada escreva() que receba um texto
 # qualquer como parametro e mostre uma mensagem com tamanho adaptável.
 
+
+def l(comprimento):
+    print('-' * comprimento)
+
+
+def escreva(texto):
+    comprimento_texto = len(texto)
+    l(comprimento_texto)
+    print(texto)
+    l(comprimento_texto)
+
+
+texto = input("Digite um texto qualquer: ")
+escreva(texto)
