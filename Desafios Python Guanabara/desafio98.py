fcefab65d3533adbbe162728dce4bed91d8d7fcf
CesarOli <cesaraleoliveira@gmail.com>
2024-04-24 16:49:39 -0300

    Enunciado desafio 98.

---
+++ Desafios Python Guanabara/desafio98.py
@@ -0,0 +1,8 @@
+# Faça um programa que tenha uma função chamada contador(), que receba três 
+# parâmetros: início, fim, passo e realize a contagem. 
+# O programa deve realizar três contagens através da função criada: 
+
+# De 1 a 10, de 1 em 1.
+# De 10 a 0, de 2 em 2.
+# Uma contagem personalizada.
+ 
