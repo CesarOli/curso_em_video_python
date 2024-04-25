6940616741d84aaad8486079af970ce70e81f360
CesarOli <cesaraleoliveira@gmail.com>
2024-04-25 14:06:56 -0300

    Desafio 98 finalizado.

--- Desafios Python Guanabara/desafio98.py
+++ Desafios Python Guanabara/desafio98.py
@@ -5,4 +5,45 @@
 # De 1 a 10, de 1 em 1.
 # De 10 a 0, de 2 em 2.
 # Uma contagem personalizada.
- 
+
+
+from time import sleep
+
+
+def contador(inicio, fim, passo):
+
+    if passo < 0:
+       passo *= -1
+    if passo == 0:
+        passo = 1
+    
+    print(f'\nContagem de {inicio} até {fim} de {passo} em {passo}.') 
+    sleep(2.5)
+
+    if inicio < fim:
+        c = inicio
+        while c <= fim:
+            print(c, end=' ', flush=True)
+            sleep(0.5)
+            c += passo
+        print('FIM!!\n')
+    else:
+        c = inicio
+        while c >= fim:
+            print(f'{c} ', end='', flush=True)
+            sleep(0.5)
+            c -= passo
+        print('FIM!!')
+
+
+#Programa Principal
+contador(0, 100, 10)
+contador(10, 0, 2)
+sleep(1)
+print("Informe e monte como gostaria de ver seu contador: ")
+i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+contador(i, f, p)
+
+
