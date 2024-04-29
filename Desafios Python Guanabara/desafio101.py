bac95585775be787a35e6675de859353d3022fa5
CesarOli <cesaraleoliveira@gmail.com>
2024-04-29 13:33:16 -0300

    Desafio 101 finalizado.

--- Desafios Python Guanabara/desafio101.py
+++ Desafios Python Guanabara/desafio101.py
@@ -1,4 +1,19 @@
-# Crie um programa que tenha uma funão chamada voto() que vai receber como 
+# Crie um programa que tenha uma função chamada voto() que vai receber como 
 # parâmetro o ano de nascimento de uma pessoa, retornando um valor literal 
-#indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.
+# indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.
 
+def voto(ano_nascimento):
+    from datetime import datetime
+    ano_atual = datetime.now().year
+    idade = ano_atual - ano_nascimento
+    if idade < 16:
+        return 'NEGADO!'`
+    elif 16 <= idade < 18 or idade >= 70:
+        return 'OPCIONAL'
+    else:
+        return 'OBRIGATÓRIO'
+    
+
+ano_nascimento = int(input('Informe o seu ano de nascimento: '))
+resultado_voto = voto(ano_nascimento)
+print(f'Seu voto é {resultado_voto} nas próximas eleições.')
