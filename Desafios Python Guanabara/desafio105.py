a4b670fc27a83f076b8999258e34876075ec4002
CesarOli <cesaraleoliveira@gmail.com>
2024-05-05 00:17:03 -0300

    Enunciado desafio 105.

---
+++ Desafios Python Guanabara/desafio105.py
@@ -0,0 +1,7 @@
+# Faça um programa que tenha uma função notas() que pode receber várias notas de
+# alunos e vai retornar um dicionário com as seguintes informações: 
+# - Quantidade de notas 
+# - A maior note - A menor nota 
+#- A média da turma - 
+# A situação (opcional)
+# - adicionar tbm o docstring da função.
