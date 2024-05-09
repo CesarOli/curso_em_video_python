e2c76155c135eb67529e4b007d6fd9f2d93d11ec
CesarOli <cesaraleoliveira@gmail.com>
2024-05-08 23:00:45 -0300

    Desafio 105 finalizado, com inclusão da classe datetime, função sleep().

--- Desafios Python Guanabara/desafio105.py
+++ Desafios Python Guanabara/desafio105.py
@@ -6,59 +6,67 @@
 # - A média da turma - 
 # - A situação (opcional)
 # - Adicionar tbm o docstring da função.
-'''
 
 # Primeira Solução
+from time import sleep
 
-def notas(*nota, situacao = False):
+def notas(*nota, situacao=False):
 
-"""
-    Calcula estatísticas de uma lista de notas e por opção do usuário retorna a situação do aluno.
-
-    Parâmetros:
-        *nota (float): Uma sequência de notas dos alunos.
-        situacao (bool, opcional): Indica se a situação do aluno deve ser incluída no resultado.
-                                    Se True, a função retorna a situação do aluno com base na média das notas.
-                                    Se False (padrão), a função não inclui a situação do aluno no resultado.
-
-    Retorna:
-        dict: Um dicionário contendo as estatísticas das notas.
-              As chaves são:
-                  - 'total': O número total de notas fornecidas.
-                  - 'maior': A maior nota dentre as fornecidas.
-                  - 'menor': A menor nota dentre as fornecidas.
-                  - 'média': A média das notas fornecidas.
-                  - 'situacao' (opcional): A situação do aluno com base na média das notas, se situacao=True.
-                                            Se situacao=False, essa chave não estará presente no dicionário.
-
-    Exemplo:
-        resultado = notas(10, 10, 8, situacao=True)
-        print(resultado)
     """
+        Calcula estatísticas de uma lista de notas e por opção do usuário retorna a situação do aluno.
+
+        Parâmetros:
+            *nota (float): Uma sequência de notas dos alunos.
+            situacao (bool, opcional): Indica se a situação do aluno deve ser incluída no resultado.
+                                        Se True, a função retorna a situação do aluno com base na média das notas.
+                                        Se False (padrão), a função não inclui a situação do aluno no resultado.
+
+        Retorna:
+            dict: Um dicionário contendo as estatísticas das notas.
+                As chaves são:
+                    - 'total': O número total de notas fornecidas.
+                    - 'maior': A maior nota dentre as fornecidas.
+                    - 'menor': A menor nota dentre as fornecidas.
+                    - 'média': A média das notas fornecidas.
+                    - 'situacao' (opcional): A situação do aluno com base na média das notas, se situacao=True.
+                                                Se situacao=False, essa chave não estará presente no dicionário.
+
+        Exemplo:
+            resultado = notas(10, 10, 8, situacao=True)
+            print(resultado)
+        """
     total_notas = len(nota)
     maior_nota = max(nota)
     menor_nota = min(nota)
     media = sum(nota) / len(nota)
 
-    if media >= 5 and media <= 7:
-        situacao = 'Sempre pode melhorar! Continue estudando.'
-    elif media < 5:
-        situacao = 'Estuda mano, ta ruim pra você.'
-    else:
-        'É isso, continue assim!'
-    
+    resultado = {'total': total_notas, 'maior': maior_nota, 'menor': menor_nota, 'média': media}
+
     if situacao:
+        if media >= 5 and media <= 7:
+            situacao = 'Sempre pode melhorar! Continue estudando.'
+        elif media < 5:
+            situacao = 'Estuda mano, tá ruim pra você.'
+        else:
+            situacao = 'É isso, continue assim!'
+        
         resultado['situacao'] = situacao
 
-    return {f'total': total_notas, 'maior': maior_nota, 'menor': menor_nota, 'média': media}
+    return resultado
 
-#Programa Principal
-resultado = notas(10, 10, 5, situacao=True)
-print(resultado)'''
+# Programa Principal
+sleep(1)
+print('Primeira Solução')
+resultado = notas(10, 0, 1.5, situacao=True)
+sleep(1)
+print(resultado)
+sleep(0.5)
+
 
 # Segunda Solução Corrigida e com a Docstring
+'''from time import sleep
 
-'''def notas(*nota, situacao=False):
+def notas(*nota, situacao=False):
 
     """
     Calcula estatísticas de uma lista de notas e por opção do usuário retorna a situação do aluno.
@@ -88,12 +96,16 @@ print(resultado)'''
     menor_nota = min(nota)
     media = sum(nota) / len(nota)
 
-    if media >= 5 and media <= 7:
-        situacao = 'Sempre pode melhorar! Continue estudando.'
-    elif media < 5:
-        situacao = 'Estuda mano, tá ruim pra você.'
+    if situacao:
+        if media >= 5 and media <= 7:
+            situacao = 'Sempre pode melhorar! Continue estudando.'
+        elif media < 5:
+            situacao = 'Estuda mano, tá ruim pra você.'
+        else:
+            situacao = 'É isso, continue assim!'
+    
     else:
-        situacao = 'É isso, continue assim!'
+        situacao = None
     
     resultado = {'total': total_notas, 'maior': maior_nota, 'menor': menor_nota, 'média': media}
 
@@ -103,14 +115,20 @@ print(resultado)'''
     return resultado
 
 # Programa Principal
-resultado = notas(10, 10, 8, situacao=True)
+sleep(1.2)
+print('Segunda Solução')
+resultado = notas(0, 0, 8, situacao=True)
+sleep(1)
 print(resultado)
+sleep(2)
+print('Fim do Programa!')
+sleep(0.5)
 '''
 
 
 # Terceira Solução, crição simples do dicionário
 
-def notas(*notas, situacao = False):
+'''def notas(*notas, situacao = False):
 
     """
     Calcula estatísticas de uma lista de notas e por opção do usuário retorna a situação do aluno.
@@ -153,4 +171,4 @@ def notas(*notas, situacao = False):
 # Programa Principal
 resposta = notas(7.5, 7.5, 10, situacao=True)
 print(resposta)
-help(notas)
+help(notas)'''
