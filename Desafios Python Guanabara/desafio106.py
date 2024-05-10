b86259e88c8b9f0ac0696316189c8adc9bc55111
CesarOli <cesaraleoliveira@gmail.com>
2024-05-09 21:39:41 -0300

    Criação da lista de cores, e importação da biblioteca datetime, função sleep().

--- Desafios Python Guanabara/desafio106.py
+++ Desafios Python Guanabara/desafio106.py
@@ -3,3 +3,13 @@
 # FIM', o programa se encerrará.
 
 #Obs: Usar cores
+from datetime import sleep
+
+cores = ('\033[m',        # 0 - sem cor
+         '\033[30;41m',   # 1 - vermelho
+         '\033[30;42m'    # 2 - verde
+         '\033[30;43m'    # 3 - amarelo
+         '\033[30;44m'    # 4 - azul
+         '\033[30;45m'    # 5 - roxo
+         '\033[7;30m'     # 6 - branco
+         )
