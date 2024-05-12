a388b47ccd04a43a1ab7d91664caa2c02d2f3942
CesarOli <cesaraleoliveira@gmail.com>
2024-05-12 20:42:01 -0300

    Correções necessárias e finalização do desafio 106.

--- Desafios Python Guanabara/desafio106.py
+++ Desafios Python Guanabara/desafio106.py
@@ -3,22 +3,22 @@
 # FIM', o programa se encerrará.
 
 #Obs: Usar cores
-from datetime import sleep
+from time import sleep
 
 cores = ('\033[m',        # 0 - sem cor
          '\033[30;41m',   # 1 - vermelho
-         '\033[30;42m'    # 2 - verde
-         '\033[30;43m'    # 3 - amarelo
-         '\033[30;44m'    # 4 - azul
-         '\033[30;45m'    # 5 - roxo
+         '\033[30;42m',   # 2 - verde
+         '\033[30;43m',    # 3 - amarelo
+         '\033[30;44m',    # 4 - azul
+         '\033[30;45m',    # 5 - roxo
          '\033[7;30m'     # 6 - branco
          )
 
 def ajuda(comando):
     titulo(f'Acessando o manual do comando \'{comando}\'', 4)
     print(cores[6], end='')
-    help(comando
-    print(c[0], end=''))
+    help(comando)
+    print(cores[0], end='')
     sleep(1.5)
 
 
@@ -32,3 +32,14 @@ def titulo(mensagem, cor=0):
     sleep(0.8)
 
 
+#Programa Principal
+
+escolha_comando = ''
+while True:
+    titulo('SISTEMA DE AJUDA E ORIENTAÇÃO PyHELP', 2)
+    escolha_comando = str(input('Escolha uma Função ou Biblioteca para obter mais informações ->: '))
+    if escolha_comando.upper() == 'FIM':
+        break
+    else:
+        ajuda(escolha_comando)
+titulo('Até Logo!!', 1)
