cbc7165ce2cf2da6501eb1ccc2011edb25c2ce38
CesarOli <cesaraleoliveira@gmail.com>
2024-05-13 16:04:55 -0300

    Inicialização do Enunciado desafio 107.

---
+++ Desafios Python Guanabara/desafio107.py
@@ -0,0 +1,6 @@
+# Crie um módulo chamado moeda.py que tenha funções incorporadas
+# aumentar(), diminuir(), dobrar() e metade().
+# Faça também um programa que importe esse mṕdilo e use alguma dessas funções.
+
+
+
