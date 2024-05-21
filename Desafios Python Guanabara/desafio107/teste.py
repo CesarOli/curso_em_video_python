92fdc5c27b1bb205c79cc694954435ac999d4d4c
CesarOli <cesaraleoliveira@gmail.com>
2024-05-21 13:38:37 -0300

    Import das funções do arquivo 'moedas.py'.

--- Desafios Python Guanabara/desafio107/teste.py
+++ Desafios Python Guanabara/desafio107/teste.py
@@ -1,9 +1,8 @@
-from moeda import dobro, aumentar, diminuir, metade
+import moeda 
 
+valor = float(input('Informe qualquer valor: '))
 
-valor = float(input('Informe qualquer valor:R$ '))
-
-print(f'Aumentando 10% de {valor}, temos {aumentar(valor, 10)}')
-print(f'Reduzindo 13% de {valor}, temos {diminuir(valor, 13)}')
-print(f'A metade de {valor}, é {metade(valor)}')
-print(f'O dobro de {valor}, é {dobro(valor)}')
+print(f'Aumentando 10% de {valor}, temos {moeda.aumentar(valor, 10)}')
+print(f'Reduzindo 13% de {valor}, temos {moeda.diminuir(valor, 13)}')
+print(f'A metade de {valor}, é {moeda.metade(valor)}')
+print(f'O dobro de {valor}, é {moeda.dobro(valor)}')
